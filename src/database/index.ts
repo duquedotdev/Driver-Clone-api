97d40175b768b8c2c2ce698a3fde753b7ddcf230
Manoel Neto <maguserv@gmail.com>
2021-10-13 12:10:10 -0300

    chore: update config typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,4 +2,7 @@ import { createConnection, useContainer } from 'typeorm';
 import { Container } from 'typeorm-typedi-extensions';
 
 useContainer(Container);
-createConnection();
+createConnection().catch((error) => {
+  console.error(`Couldn't connect to the database!`);
+  console.error(error);
+});
